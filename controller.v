be66191f806888093c878ec029587cb34cf25e56
Ryan Plante <ryan.plante@maine.edu>
2018-10-29 13:29:12 -0400

    Added better comments

--- controller.v
+++ controller.v
@@ -1,3 +1,7 @@
+/* A big state machine, since we are only implementing R-type instructions
+	we only have a few, relatively simple states. These will be expanded in the future
+*/
+
 module controller();
 
 endmodule 
