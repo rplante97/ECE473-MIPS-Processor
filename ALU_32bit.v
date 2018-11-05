221d6e059d95fce0fd15aa25919fd83cda29bc43
Ryan Plante <rplante97@gmail.cim>
2018-11-05 18:21:20 -0500

    Fixed latching behavior on ALU

--- ALU_32bit.v
+++ ALU_32bit.v
@@ -13,47 +13,38 @@ input [4:0] shamt;
 output reg zero; //We set zero to high if ALU_result equals zero
 output reg [31:0] ALU_result;
 
-//clock on new signals from data1 and data2 or control
-always @(data1, data2, ALU_operation) begin
+always begin
 	
 	case(ALU_operation)
 	
 		//TODO: IMPLEMENTATION
 		32: //ADD
-			ALU_result <= data1 + data2;
+			ALU_result = data1 + data2;
 		33: //ADDU
-			ALU_result <= data1 + data2;
+			ALU_result = data1 + data2;
 		34: //SUB
-			ALU_result <= data1 - data2;
+			ALU_result = data1 - data2;
 		35: //SUBU
-			ALU_result <= data1 - data2;
+			ALU_result = data1 - data2;
 		36: //AND
-			ALU_result <= data1 & data2;
+			ALU_result = data1 & data2;
 		37: //OR
-			ALU_result <= data1 | data2;
+			ALU_result = data1 | data2;
 		39: //NOR
-			ALU_result <= ~(data1 | data2);
+			ALU_result = ~(data1 | data2);
 		42: //SLT
-			ALU_result <= ($signed(data1) < $signed(data2)) ? 1 : 0;
+			ALU_result = ($signed(data1) < $signed(data2)) ? 1 : 0;
 		0: //SLL
-			ALU_result <= (data2 << shamt);
+			ALU_result = (data2 << shamt);
 		2: //SRL
-			ALU_result <= (data2 >> shamt);
+			ALU_result = (data2 >> shamt);
 		3: //SRA
-			ALU_result <= (data2 >> shamt); //Need to account for sign shifting later!!!
-		8: //jr
-			ALU_result <= ALU_result;
-		
-		
-				
-		
+			ALU_result = (data2 >> shamt); //Need to account for sign shifting later!!!
+		default:
+			ALU_result = 0;
 		
 	endcase 
-	
-	
-	
-	
-	
+		
 end
 
 endmodule 
