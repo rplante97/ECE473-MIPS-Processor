f5078d660962e76c9696f8f1c79f10af3911b2b3
Ryan Plante <ryan.plante@maine.edu>
2018-11-05 11:08:05 -0500

    Hazard detection and forwarding unit started

---
+++ hazard_detection.v
@@ -0,0 +1,10 @@
+module hazard_detection(
+
+//input previous instruction pipeline regwrite flag
+//input previous instruction pipeline destination register
+//input ex/mem alu data lines
+
+
+);
+
+endmodule 
