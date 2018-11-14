cb6fd89fff67fdc63d80ce7ed0e50768693a8163
Ryan Plante <ryan.plante@maine.edu>
2018-11-13 19:21:35 -0500

    Almost working test program 2 milestone 2

--- data_forwarding.v
+++ data_forwarding.v
@@ -24,6 +24,12 @@ module data_forwarding(
 	//The full instruction, to determine instruction type
 	input [31:0] full_ins,
 	
+	input [31:0] mem_data,
+	input [4:0] current_write_addr,
+	input mem_load,
+	input [31:0] din2,
+	
+	
 	
 	
 	//Outputs:
@@ -31,7 +37,8 @@ module data_forwarding(
 	
 	//Data output to alu, with changed values if neccassary
 	output reg [31:0] data_out1,
-	output reg [31:0] data_out2
+	output reg [31:0] data_out2,
+	output reg [31:0] dout2
 
 
 );
@@ -47,34 +54,36 @@ if (opcode == 0) begin
 		data_out2 =  data_in2;
 		
 	end
-	else if (rt == dest_register) begin
-		//Foward the alu result to rt (data_out2)
-		data_out1 = data_in1;
-		data_out2 = aluResult;
-		
-	end
 	else begin //Here we don't need to make any changes, just forward the outputs
-		
+	
 		data_out1 = data_in1;
 		data_out2 =  data_in2;
 		
 	end
-	
+	dout2 = din2;
 end
 	
 if (opcode != 0) begin
-	//if (rt == dest_register_wb) begin
-		//data_out1 = aluResult_wb;
-		//data_out2 = data_in2;
-	//end
-	//else begin //Here we don't need to make any changes, just forward the outputs
+		//If write address of instruction ahead is same as our dest register, grab the value
+		if(current_write_addr == dest_register && mem_load) begin //If we just loaded data to this register, grab it
+			data_out1 = mem_data;
+			data_out2 =  data_in2;
+			dout2 = din2;
+		end
+		//if register address we got dout2 from (rt) is same as reg address we just wrote to, grab updated value
+		else if(rt == dest_register) begin
+			dout2 = aluResult;
+			data_out1 = data_in1;
+			data_out2 =  data_in2;
+		end
 		
-		data_out1 = data_in1;
-		data_out2 =  data_in2;
-		
-	//end
-end
-
+		else begin
+			//Most cases we have no issue, just forward the outputs
+			data_out1 = data_in1;
+			data_out2 =  data_in2;
+			dout2 = din2;
+		end
 end
 	
+end
 endmodule 
